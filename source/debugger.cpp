98bdac6545af9ddb8138eda1a4fdabe00e6a620e
Johnny Bueti <29045904+Fehniix@users.noreply.github.com>
2022-12-22 10:56:43 +0100

    feat: removed auto dmnt:cht:attach + custom eps

--- source/debugger.cpp
+++ source/debugger.cpp
@@ -4,155 +4,192 @@
 #include "util.hpp"
 
 Debugger::Debugger() {
-    std::string error;
-    this->initialize(error);
+	std::string error;
+	this->initialize(error);
 }
 
 Debugger::~Debugger() {
-    if (!m_dmnt) {
-        svcContinueDebugEvent(m_debugHandle, 4 | 2 | 1, 0, 0);
-        svcCloseHandle(m_debugHandle);
-    }
-
-    pminfoExit();
-    pmdmntExit();
-    dmntchtExit(); // need to check if dmntcht is runnnig
+	if (!m_dmnt) {
+		svcContinueDebugEvent(m_debugHandle, 4 | 2 | 1, 0, 0);
+		svcCloseHandle(m_debugHandle);
+	}
 }
 
 Result Debugger::initialize(std::string& error) {
-    Result rc;
+	m_pid = 0;
+	m_tid = 0;
 
-    if (this->dmntRunning()) {
-        Result rc = dmntchtInitialize();
+	Result rc = pmdmntGetApplicationProcessId(&m_pid);
+	if (R_FAILED(rc)) {
+		error = "Could not get application process ID: " + std::to_string(rc);
+		return (Result)1;
+	}
 
-        if (R_FAILED(rc)) {
-            error = "dmntchtInitialize failed: " + std::to_string(rc);
-            return (Result)1;
-        }
-    } else {
-        error = "dmnt not running.";
-        return (Result)1;
-    }
+	rc = pminfoGetProgramId(&m_tid, m_pid);
+	if (R_FAILED(rc)) {
+		error = "Could not get program ID: " + std::to_string(rc);
+		return (Result)1;
+	}
 
-    m_pid = 0;
-    m_tid = 0;
-
-    rc = pmdmntGetApplicationProcessId(&m_pid);
-    if (R_FAILED(rc)) {
-        error = "Could not get application process ID: " + std::to_string(rc);
-        return (Result)1;
-    }
-
-    rc = pminfoGetProgramId(&m_tid, m_pid);
-    if (R_FAILED(rc)) {
-        error = "Could not get program ID: " + std::to_string(rc);
-        return (Result)1;
-    }
-
-    error = "all is well?";
-    return (Result)0;
+	return (Result)0;
 }
 
 Result Debugger::attachToProcess() {
-    if (this->m_debugHandle == 0 && (envIsSyscallHinted(0x60) == 1)) {
-        this->m_rc = svcDebugActiveProcess(&this->m_debugHandle, this->m_pid);
-        if ((int)this->m_rc == 0)
-            this->m_dmnt = false;
-        return m_rc;
-    }
-    return 1;
+	if (this->m_debugHandle == 0 && (envIsSyscallHinted(0x60) == 1)) {
+		this->m_rc = svcDebugActiveProcess(&this->m_debugHandle, this->m_pid);
+		if ((int)this->m_rc == 0)
+			this->m_dmnt = false;
+		return m_rc;
+	}
+	return 1;
   // return dmntchtForceOpenCheatProcess();
 }
 
 void Debugger::detach() {
-    if (!m_dmnt) {
-        svcContinueDebugEvent(m_debugHandle, 4 | 2 | 1, 0, 0);
-        svcCloseHandle(m_debugHandle);
-        m_dmnt = true;
-    }
+	if (!m_dmnt) {
+		svcContinueDebugEvent(m_debugHandle, 4 | 2 | 1, 0, 0);
+		svcCloseHandle(m_debugHandle);
+		m_dmnt = true;
+	}
 }
 
 bool Debugger::dmntRunning() {
-    // Get all process ids
-    u64 process_ids[0x50];
-    s32 num_process_ids;
+	// Get all process ids
+	u64 process_ids[0x50];
+	s32 num_process_ids;
 
-    Result rc = svcGetProcessList(&num_process_ids, process_ids, sizeof process_ids);  // need to double check
-    if (R_FAILED(rc))
-        return false;
+	Result rc = svcGetProcessList(&num_process_ids, process_ids, sizeof process_ids);  // need to double check
+	if (R_FAILED(rc))
+		return false;
 
-    // Look for dmnt or dmntgen2 titleID
-    u64 titleID;
-    for (s32 i = 0; i < num_process_ids; ++i)
-        if (R_SUCCEEDED(pminfoGetProgramId(&titleID, process_ids[i])))
-            if (titleID == 0x010000000000000D)
-                return true;
+	// Look for dmnt or dmntgen2 titleID
+	u64 titleID;
+	for (s32 i = 0; i < num_process_ids; ++i)
+		if (R_SUCCEEDED(pminfoGetProgramId(&titleID, process_ids[i])))
+			if (titleID == 0x010000000000000D)
+				return true;
 
-    return false;
+	return false;
 };
 
+Result Debugger::refreshMetaData(std::string &error) {
+	Result rc;
+
+	rc = dmntchtInitialize();
+	if (R_FAILED(rc)) {
+		error = "Could not initialize dmntcht, error: " + iths(rc);
+		return rc;
+	}
+
+	rc = dmntchtForceOpenCheatProcess();
+	if (R_FAILED(rc)) {
+		error = "Could not force open cheat process (dmnt:cht, forceOpenCheatProcess): " + iths(rc);
+		dmntchtExit();
+		return rc;
+	}
+
+	rc = dmntchtGetCheatProcessMetadata(&this->meta);
+	if (R_FAILED(rc)) 
+		error = "Could not fetch process metadata, error: " + iths(rc);
+
+	dmntchtExit();
+
+	return rc;
+}
+
 u64 Debugger::getRunningApplicationTID() {
-    return m_tid;
+	return m_tid;
 }
 
 u64 Debugger::getRunningApplicationPID() {
-    return m_pid;
+	return m_pid;
 }
 
 u64 Debugger::peekMemory(u64 address) {
-    u64 out;
+	u64 out;
 
-    if (m_dmnt)
-        dmntchtReadCheatProcessMemory(address, &out, sizeof(u64));
-    else
-        svcReadDebugProcessMemory(&out, m_debugHandle, address, sizeof(u64));
-    
-    return out;
+	if (m_dmnt)
+		dmntchtReadCheatProcessMemory(address, &out, sizeof(u64));
+	else
+		svcReadDebugProcessMemory(&out, m_debugHandle, address, sizeof(u64));
+	
+	return out;
 }
 
 Result Debugger::pokeMemory(size_t varSize, u64 address, u64 value) {
-    if (m_dmnt)
-        return dmntchtWriteCheatProcessMemory(address, &value, varSize);
-    else
-        return svcWriteDebugProcessMemory(m_debugHandle, &value, address, varSize);
+	if (m_dmnt)
+		return dmntchtWriteCheatProcessMemory(address, &value, varSize);
+	else
+		return svcWriteDebugProcessMemory(m_debugHandle, &value, address, varSize);
 }
 
 Result Debugger::pause() {
-    if (m_dmnt)
-        return dmntchtPauseCheatProcess();
-    else
-        return svcBreakDebugProcess(m_debugHandle);
+	if (m_dmnt)
+		return dmntchtPauseCheatProcess();
+	else
+		return svcBreakDebugProcess(m_debugHandle);
 }
 
 Result Debugger::resume() {
-    if (m_dmnt)
-        return dmntchtResumeCheatProcess();
-    else
-        return svcContinueDebugEvent(m_debugHandle, 4 | 2 | 1, 0, 0);
+	if (m_dmnt)
+		return dmntchtResumeCheatProcess();
+	else
+		return svcContinueDebugEvent(m_debugHandle, 4 | 2 | 1, 0, 0);
 }
 
 Result Debugger::readMemory(void *buffer, size_t bufferSize, u64 address) {
-    if (m_dmnt)
-        return dmntchtReadCheatProcessMemory(address, buffer, bufferSize);
-    else
-        return svcReadDebugProcessMemory(buffer, m_debugHandle, address, bufferSize);
+	if (m_dmnt)
+		return dmntchtReadCheatProcessMemory(address, buffer, bufferSize);
+	else
+		return svcReadDebugProcessMemory(buffer, m_debugHandle, address, bufferSize);
 }
 
 Result Debugger::writeMemory(void *buffer, size_t bufferSize, u64 address) {
-    if (m_dmnt)
-        return dmntchtWriteCheatProcessMemory(address, buffer, bufferSize);
-    else
-        return svcWriteDebugProcessMemory(m_debugHandle, buffer, address, bufferSize);
+	if (m_dmnt)
+		return dmntchtWriteCheatProcessMemory(address, buffer, bufferSize);
+	else
+		return svcWriteDebugProcessMemory(m_debugHandle, buffer, address, bufferSize);
 }
 
 MemoryInfo Debugger::queryMemory(u64 address) {
-    MemoryInfo memInfo = { 0 };
-    if (m_dmnt)
-        dmntchtQueryCheatProcessMemory(&memInfo, address);
-    else {
-        u32 pageinfo; // ignored
-        svcQueryDebugProcessMemory(&memInfo, &pageinfo, m_debugHandle, address);
-    }
+	MemoryInfo memInfo = { 0 };
+	if (m_dmnt)
+		dmntchtQueryCheatProcessMemory(&memInfo, address);
+	else {
+		u32 pageinfo; // ignored
+		svcQueryDebugProcessMemory(&memInfo, &pageinfo, m_debugHandle, address);
+	}
 
-    return memInfo;
+	return memInfo;
+}
+
+/* 
+* ------------
+* GETTERS
+* ------------
+*/
+
+/**
+ * @brief Gets the currently running application's heap start & size.
+ * 
+ * @return const DmntMemoryRegionExtents& 
+ */
+const DmntMemoryRegionExtents& Debugger::getHeap() const {
+	return this->meta.heap_extents;
+}
+
+const DmntMemoryRegionExtents& Debugger::getMain() const {
+	return this->meta.main_nso_extents;
+}
+
+u64 Debugger::getPID() {
+	return this->meta.process_id;
+}
+
+u64 Debugger::getTitleID() {
+	return this->meta.title_id;
+}
+
+const u8* Debugger::getNsoBuildId() const {
+	return this->meta.main_nso_build_id;
 }
--- source/debugger.hpp
+++ source/debugger.hpp
@@ -2,29 +2,41 @@
 
 #include <iostream>
 #include <switch.h>
+#include "dmntcht.h"
 
 class Debugger {
-public:
-  Debugger();
-  ~Debugger();
+	public:
+		bool m_dmnt = true;
+		Result m_rc = 0;
+		Handle m_debugHandle = 0; // no action to be taken to attach
 
-  Result initialize(std::string &error);
-  Result attachToProcess();
-  Result pause();
-  Result resume();
-  void detach();
-  bool dmntRunning();
-  u64 getRunningApplicationTID();
-  u64 getRunningApplicationPID();
-  bool m_dmnt = true;
-  Result m_rc = 0;
-  Handle m_debugHandle = 0; // no action to be taken to attach
-  u64 peekMemory(u64 address);
-  Result pokeMemory(size_t varSize, u64 address, u64 value);
-  MemoryInfo queryMemory(u64 address);
-  Result readMemory(void *buffer, size_t bufferSize, u64 address);
-  Result writeMemory(void *buffer, size_t bufferSize, u64 address);
+		Debugger();
+		~Debugger();
 
-private:
-  u64 m_tid = 0, m_pid = 0;
+		Result initialize(std::string &error);
+		Result attachToProcess();
+		Result pause();
+		Result resume();
+		void detach();
+		bool dmntRunning();
+
+		Result refreshMetaData(std::string &error);
+		u64 getRunningApplicationTID();
+		u64 getRunningApplicationPID();
+
+		u64 peekMemory(u64 address);
+		Result pokeMemory(size_t varSize, u64 address, u64 value);
+		MemoryInfo queryMemory(u64 address);
+		Result readMemory(void *buffer, size_t bufferSize, u64 address);
+		Result writeMemory(void *buffer, size_t bufferSize, u64 address);
+
+		const DmntMemoryRegionExtents& getHeap() const;
+		const DmntMemoryRegionExtents& getMain() const;
+		u64 getPID();
+		u64 getTitleID();
+		const u8* getNsoBuildId() const;
+
+	private:
+		u64 m_tid = 0, m_pid = 0;
+		DmntCheatProcessMetadata meta;
 };

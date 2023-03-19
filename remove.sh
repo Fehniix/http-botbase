7b64693831fbf9a763f5a74b4810c9ee25783aa9
Johnny Bueti <29045904+Fehniix@users.noreply.github.com>
2023-03-19 13:20:25 -0400

    feat: added scripts to autocompile & push

---
+++ remove.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Replace current sys-module on Switch SD FS with build output
+
+[ ! -d "/Volumes/SWITCH SD" ] && echo "Switch SD not mounted." && exit -1
+
+if [ ! -d "/Volumes/SWITCH SD/atmosphere/contents/410000000000FF15/" ]
+then
+	echo "Previous build on Switch FS not found. Nothing to remove."
+	exit
+fi
+
+rm -r "/Volumes/SWITCH SD/atmosphere/contents/410000000000FF15/"
+echo "Previous build removed from Switch FS."
+
+SWITCH_VOLUME=$(diskutil list | grep "SWITCH SD" | grep -ow "disk.*$" | perl -pe "chomp")
+
+if [ -z $SWITCH_VOLUME ]
+then
+	echo "Switch SD volume could not be automatically fetched, need to unmount manually. Exiting."
+	exit 0
+else
+	diskutil eject "/dev/$SWITCH_VOLUME"
+	echo "Switch SD volume ejected."
+fi
